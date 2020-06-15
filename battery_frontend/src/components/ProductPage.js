0f6d0f698ee12e999f73997073fcef7529d20e46
Animesh <animeshg93@utexas.edu>
2020-06-14 18:54:04 -0700

    Refactored the layout of productpage

--- battery_frontend/src/components/ProductPage.js
+++ battery_frontend/src/components/ProductPage.js
@@ -13,12 +13,21 @@ export default function ProductPage(props){
 	},{})
 
 	return(
-		<div>
+		<div className={styles.productContainer}>
 			<div className={styles.imageContainer}>
 				<img className={styles.image} src={battery.image} />
 			</div>
 			<div className={styles.title}>
 				<h1>{battery.name}</h1>
+				<h3>Price: {battery.price}</h3>
+			</div>
+			<div className={styles.price}>
+				<select id="cars" name="cars">
+				    <option value="1">1</option>
+				    <option value="2">2</option>
+				    <option value="3">3</option>
+				    <option value="4">4</option>
+				</select>
 			</div>
 		</div>
 	);
