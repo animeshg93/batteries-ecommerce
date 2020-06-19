6d136e59d490247277b095fffa2322fc9059cb1f
Animesh <animeshg93@utexas.edu>
2020-06-18 22:11:14 -0700

    Added shopping cart and quantity display logic

--- battery_frontend/src/components/Banner.js
+++ battery_frontend/src/components/Banner.js
@@ -3,14 +3,12 @@ import styles from '../css/banner.module.css'
 
 export default function Banner(){
 	return(
-		<div className={styles.nav}>
-			<ul className={styles.left}>
-				<li><h1>ANIMESH BATTERIES</h1></li>
-			</ul>
-			<ul className={styles.right}>
-				<li><img className={styles.cart} src="https://cdn0.iconfinder.com/data/icons/shopping-cart-26/1000/Shopping-Basket-02-512.png" /></li>
-			</ul>
-			
+		<div className={styles.topNav}>
+			<a className={styles.brand} href="/batteries"><h1>ANIMESH BATTERIES</h1></a>
+			<div className={styles.right}>
+				<span className={styles.cart}></span>
+			</div>
+			<span className={styles.counter}>19</span>
 		</div>
 	);
 }
--- data-loader.py
+++ data-loader.py
@@ -2,12 +2,13 @@ import redis
 
 r = redis.StrictRedis(charset="utf-8", decode_responses=True)
 
+r.flushdb();
+
 r.hmset("AmazonBasics-12-Ct",{"image":"https://m.media-amazon.com/images/I/81rQOqg6S3L._AC_UL320_.jpg", "name":"AmazonBasics 1.5V 12 Ct","price":"$12.99","quantity":200})
 r.hmset("Energizer-48-Ct",{"image":"https://m.media-amazon.com/images/I/81S+pX7uujL._AC_UL320_.jpg", "name":"Energizer AAA 48 Ct","price":"$13.58","quantity":250})
 r.hmset("Duracell-4-Ct",{"image":"https://m.media-amazon.com/images/I/71B+sFbk3FL._AC_UL320_.jpg", "name":"Duracell 2032 3V 4 Ct","price":"$7.56","quantity":100})
-# r.hmset("",)
-# r.hmset("",)
-# r.hmset("",)
-# r.hmset("",)
-# r.hmset("",)
-# r.hmset("",)
+
+r.hmset("AmazonBasics-Coin-2-Ct",{"image":"https://images-na.ssl-images-amazon.com/images/I/61WfS6xHLxL._AC_UL200_SR200,200_.jpg", "name":"AmazonBasics CR1616 Coin 2 Ct","price":"$3.51","quantity":45})
+r.hmset("Energizer-Coin-2-Ct",{"image":"https://images-na.ssl-images-amazon.com/images/I/819A0g-%2BaJL._AC_UL200_SR200,200_.jpg", "name":"Energizer 2016 2 Ct","price":"$2.44","quantity":1000})
+r.hmset("LiCB-Coin-20-Ct",{"image":"https://images-na.ssl-images-amazon.com/images/I/81zyi4tOtYL._AC_UL200_SR200,200_.jpg", "name":"LiCB LR44 Coin Cell 20 Ct","price":"$5.00","quantity":8457})
+
